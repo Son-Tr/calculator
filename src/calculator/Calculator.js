2fd1cda8c70e171eb07e1ec232b74f19eee1e839
Son-Tr <sontran.kh1502@gmail.com>
2024-09-02 15:44:55 +0700

    update

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -17,8 +17,8 @@ export default class Calculator extends Component {
     let { number } = this.state;
     num = num.toString();
 
-     // Prevent adding multiple decimal points
-     if (num === '.' && number.includes('.')) {
+    // Prevent adding multiple decimal points
+    if (num === '.' && number.includes('.')) {
       return;
     }
 
@@ -29,42 +29,41 @@ export default class Calculator extends Component {
       number += num;
     }
 
-     // limmit the length of the number to 13 character in case the number is decimal point
-    if(number.includes(".") && number.length > 14){
-      number= number.slice(0,14);
+    // limmit the length of the number to 13 character in case the number is decimal point
+    if (number.includes(".") && number.length > 14) {
+      number = number.slice(0, 14);
       alert("Limmit the length of number is 13 characters")
     }
 
     // limmit the length of the number to 13 character
-    if ( !number.includes(".") && number.length > 13){
-     number= number.slice(0,13);
-     alert("Limmit the length of number is 13 characters")
+    if (!number.includes(".") && number.length > 13) {
+      number = number.slice(0, 13);
+      alert("Limmit the length of number is 13 characters")
     }
-   
+
     // Update the state with the formatted number,Call updateFontSize after setting state
     this.setState({ number }, this.updateFontSize);
   };
 
-  c
 
   /* ------------------------------ fotmat number ----------------------------- */
-    formatNumber = (number) => {
+  formatNumber = (number) => {
 
-      let [integerPart, decimalPart] = number.split(".");
+    let [integerPart, decimalPart] = number.split(".");
 
-      //add to comman to the integer part 
-      integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    //add to comma to the integer part 
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-      //if decimal part return the formatted integer with decimal part
-      if(decimalPart!== undefined){
-        return `${integerPart}.${decimalPart}`
-      }
+    //if decimal part return the formatted integer with decimal part
+    if (decimalPart !== undefined) {
+      return `${integerPart}.${decimalPart}`
+    }
 
-      // return just the formatted integer if there isnt decimal part 
-      return integerPart;
-    };
+    // return just the formatted integer if there isnt decimal part 
+    return integerPart;
+  };
 
-
+  /* ------------- fix the font-size base on the length of number ------------- */
   updateFontSize = () => {
     const { number } = this.state;
     let fontSizeClass = 'normal-font';
@@ -76,26 +75,53 @@ export default class Calculator extends Component {
     this.setState({ fontSizeClass });
   };
 
-  render() {
-    const { number, fontSizeClass } = this.state;
-    const formattedNumber = this.formatNumber(number);
+  /* ---------------------------- handle click back space  --------------------------- */
+  hanldeBackSpace =()=>{
+    let {number} = this.state;
+    if(number.length < 1){
+      number= "0"
+    }else{
+      number = number.slice(0,-1)
+    }
+
+    // if the result is an emtry string . reset to "0"
+    if (number === '') {
+      number = '0';
+    }
     
+    this.setState({number}, this.updateFontSize)
+  }
+
+  render() {
+    const { number, fontSizeClass, preNumber, operation } = this.state;
+    const formattedNumber = this.formatNumber(number);
+
 
     return (
       <div className='container'>
         <h1 className='title'>Calculator</h1>
         <div className="box">
           <div className="input-results">
-            <p className='formula'>62<span className='operation'>x</span></p>
+            <p className='formula'>
+              {preNumber}
+              <span className='operation'>
+                {operation}
+              </span>
+            </p>
             <p className={`output no-spinner ${fontSizeClass}`} id='display'>
-              {formattedNumber === "" ? '0' : formattedNumber}
+              {formattedNumber}
             </p>
           </div>
           <div className="grid-btn">
-            <button id="clear" onClick={() => this.setState({ number: '0' })}>AC</button>
+            <button id="clear" onClick={() => this.setState({
+              preNumber: 0,
+              number: '0',
+              operation: '',
+              fontSizeClass: 'normal-font',
+            })}>AC</button>
             <button id="percent">%</button>
             <button id="divide"><i className="fa-solid fa-divide" /></button>
-            <button id="back-space"><i className="fa-solid fa-delete-left" /></button>
+            <button id="back-space" onClick={this.hanldeBackSpace}><i className="fa-solid fa-delete-left" /></button>
             <button id="seven" onClick={() => this.handleClickNum(7)}>7</button>
             <button id="eight" onClick={() => this.handleClickNum(8)}>8</button>
             <button id="nine" onClick={() => this.handleClickNum(9)}>9</button>
